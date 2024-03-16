a22b04c8bec371cdf84287f49436e66dac98e8d9
JosephYuson <yuson.josephanthony@gmail.com>
2024-03-16 17:49:10 +0800

    First Commit

---
+++ it-sysarch32-78730-activity3/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './Header'
+import Pokedex from './Pokedex'
+
+function App() {
+  return (
+    <>
+    <div className='App2'>
+      <Header/>
+    </div>
+    <div className="App">
+      <Pokedex />
+    </div>
+    </>
+  );
+}
+
+export default App;
